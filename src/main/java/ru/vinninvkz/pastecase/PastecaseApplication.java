8f0aecd98fa0c06fae621348b82f03bc92ce1bd2
Vlad <vladzh0610@gmail.com>
2024-02-28 17:45:31 +0300

    init project

---
+++ src/main/java/ru/vinninvkz/pastecase/PastecaseApplication.java
@@ -0,0 +1,13 @@
+package ru.vinninvkz.pastecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastecaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastecaseApplication.class, args);
+	}
+
+}
